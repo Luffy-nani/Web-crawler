494732c75a3f8da4333acbe2b720627a950427f3
Luffy-nani <venkatedupuganti0311@gmail.com>
2026-06-08 17:16:44 +0000

    Crawler is working

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
+var url = "https://books.toscrape.com"
+var m = make(map[string]bool)
+
 func main() {
-	url := "https://books.toscrape.com"
+	crawl(url)
+}
+
+func crawl(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
@@ -18,16 +25,39 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	m[url]=true
 	walk(docs)
-
 }
 
 func walk(node *html.Node) {
 	if node.Type == html.ElementNode && node.Data == "a" {
 		for _, attr := range node.Attr {
 			if attr.Key == "href" {
-				fmt.Println("url: ", attr.Val)
+				baseUrl := attr.Val
+				if strings.Contains(baseUrl, "..") {
+					continue
+				}
+				if strings.HasPrefix(baseUrl, "https://") {
+					if m[baseUrl] != true {
+						fmt.Println("url: ", baseUrl)
+						m[baseUrl] = true
+						crawl(baseUrl)
+					}
+				} else if strings.HasPrefix(baseUrl, "/") {
+					baseUrl = url + baseUrl
+					if m[baseUrl] != true {
+						fmt.Println("url: ", baseUrl)
+						m[baseUrl] = true
+						crawl(baseUrl)
+					}
+				} else {
+					baseUrl = url + "/" + baseUrl
+					if m[baseUrl] != true {
+						fmt.Println("url: ", baseUrl)
+						m[baseUrl] = true
+						crawl(baseUrl)
+					}
+				}
 			}
 		}
 	}
